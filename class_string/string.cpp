cace01801003a01518c632e27cc1941e1cadd42a
filian <filiandun@gmail.com>
2022-11-27 15:05:42 +0300

    class String (2): complete

--- class_string/string.cpp
+++ class_string/string.cpp
@@ -1,1 +1,77 @@
 #include "string.h"
+
+short int String::object_counter = 0;
+
+// ÊÎÍÑÒÐÓÊÒÎÐ - 80
+String::String()
+{
+	this->string_length = 80;
+	this->string = new char[this->string_length];
+
+	++object_counter;
+}
+
+// ÊÎÍÑÒÐÓÊÒÎÐ - ÏÐÎÈÇÂÎËÜÍÀß ÄËÈÍÀ
+String::String(long long int string_length)
+{
+	this->string_length = string_length;
+	this->string = new char[string_length];
+
+	++object_counter;
+}
+
+// ÊÎÍÑÒÐÓÊÒÎÐ - ÑÒÐÎÊÀ ÏÎËÜÇÎÂÀÒÅËß
+String::String(char* user_string)
+{
+	long long int string_length = 0;
+
+	do
+	{
+		++string_length;
+	} while (user_string[string_length] != '\0');
+
+	this->string_length = string_length;
+
+
+	this->string = new char[this->string_length];
+
+	for (int i = 0; i < this->string_length; i++)
+	{
+		this->string[i] = user_string[i];
+	}
+
+	++object_counter;
+}
+
+// ÄÅÑÒÐÓÊÒÎÐ
+String::~String()
+{
+	delete[] this->string;
+}
+
+
+void String::set_string(char* user_string)
+{
+	for (int i = 0; i < string_length; i++)
+	{
+		this->string[i] = user_string[i];
+	}
+}
+
+
+void String::display_string() const
+{
+	std::cout << "Ñòðîêà: ";
+	for (int i = 0; i < this->string_length; ++i)
+	{
+		std::cout << this->string[i];
+	}
+}
+
+
+
+
+
+
+
+
--- class_string/string.h
+++ class_string/string.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <iostream>
+
 class String
 {
+public:
+	static short int object_counter; 
+
+	String(); // конструктор -  80
+	String(long long int string_length); // конструктор - произвольная длина
+	String(char* user_string); // конструктор - строка пользователя
+
+	~String(); // деструктор
+
+private:
+	char* string = nullptr;
+	long long int string_length = 0;
+
+public:
+	void set_string(char* user_string);
+	void display_string() const;
+
 };
 
