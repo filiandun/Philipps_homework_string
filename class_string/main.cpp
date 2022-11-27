5f37160d961adf21d760941bc162524493dc1e1b
filian <filiandun@gmail.com>
2022-11-27 15:06:07 +0300

    main.cpp (1): complete

--- class_string/main.cpp
+++ class_string/main.cpp
@@ -1,6 +1,25 @@
 ﻿#include <iostream>
+#include "string.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+
+// КОНСТРУКТОР - 80
+	String* string_1 = new String();
+
+	
+// КОНСТРУКТОР - ПРОИЗВОЛЬНАЯ ДЛИНА
+	String* string_2 = new String(200);
+
+
+// КОНСТРУКТОР - СТРОКА ПОЛЬЗОВАТЕЛЯ
+	char* user_string = new char[5] { 'C', 'H', 'A', 'R', '\0' };
+	String* string_3 = new String(user_string);
+
+	string_3->display_string();
+
+	std::cout << std::endl << "Кол-во созданных объектов: " << string_3->object_counter;
+
+	return 0;
 }
